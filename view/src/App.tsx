126864a6f01041119edfef110067c2cf1a96e968
ziyuan <hanziyuan08@outlook.com>
2021-04-13 23:20:41 +0800

    前端

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,45 +1,77 @@
 import React, {FC} from 'react';
 import './App.less';
-import {Layout, message, Upload} from 'antd';
-import {InboxOutlined} from '@ant-design/icons';
-import {UploadChangeParam} from "antd/es/upload/interface";
+import {Button, Layout, Select, Table, Tag} from 'antd';
+import {UploadOutlined, RightOutlined} from '@ant-design/icons';
 import {Content, Header} from "antd/es/layout/layout";
 
-const {Dragger} = Upload;
+const {Option} = Select;
 
-const props = {
-    name: 'file',
-    multiple: true,
-    action: '/upload',
-    onChange(info: UploadChangeParam) {
-        const {status} = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
+const columns = [
+    {
+        title: '文件名',
+        dataIndex: 'name',
+        key: 'name',
+        render(text: string) {
+            return <a href="/#">{text}</a>;
+        },
     },
-};
+    {
+        title: '目标格式',
+        dataIndex: 'type',
+        key: 'type',
+        render() {
+            return <>
+                <Select defaultValue="png" style={{width: 120}}>
+                    <Option value="png">png</Option>
+                    <Option value="jpg">jpg</Option>
+                    <Option value="jpeg">jpeg</Option>
+                </Select>
+            </>
+        },
+    },
+    {
+        title: '状态',
+        key: 'status',
+        dataIndex: 'status',
+        render(status: string) {
+            let color = status.length > 5 ? 'geekblue' : 'green';
+            if (status === 'loser') {
+                color = 'volcano';
+            }
+            return (
+                <Tag color={color} key={status}>
+                    {status.toUpperCase()}
+                </Tag>
+            );
+        },
+    }
+];
+
+const data = [
+    {
+        key: '1',
+        name: '1.jpg',
+        status: '未上传',
+    }
+];
+
 const App: FC = () => (
     <Layout>
         <Header>
             <div className="logo"/>
             <h1 className="page-title">在线图像文件转换器</h1>
         </Header>
-        <Content style={{padding: '0 50px'}}>
-            <Dragger {...props}>
-                <p className="ant-upload-drag-icon">
-                    <InboxOutlined/>
-                </p>
-                <p className="ant-upload-text">点击或拖拽文件到该区域上传</p>
-                <p className="ant-upload-hint">
-                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-                    band files
-                </p>
-            </Dragger>
+        <Content className="content">
+            <Button type="primary" danger icon={<UploadOutlined />} size="large" className="choose-button">
+                选择文件
+            </Button>
+            <input type="file" id="upload-add" style={{display: 'none'}}/>
+
+            <Table columns={columns} dataSource={data} className="table" pagination={false}/>
+
+            <Button type="primary" icon={<RightOutlined />} size="large" className="upload-button">
+                开始转换
+            </Button>
         </Content>
     </Layout>
 );
