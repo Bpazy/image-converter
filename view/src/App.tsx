d832d647611cb036858cfc8525bd4c9a5a195e8c
ziyuan <hanziyuan08@outlook.com>
2021-04-14 21:25:46 +0800

    抽组件

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,59 +1,8 @@
 import React, {FC} from 'react';
 import './App.less';
-import {Button, Layout, Select, Table, Tag} from 'antd';
-import {UploadOutlined, RightOutlined} from '@ant-design/icons';
+import {Layout} from 'antd';
 import {Content, Header} from "antd/es/layout/layout";
-
-const {Option} = Select;
-
-const columns = [
-    {
-        title: '文件名',
-        dataIndex: 'name',
-        key: 'name',
-        render(text: string) {
-            return <a href="/#">{text}</a>;
-        },
-    },
-    {
-        title: '目标格式',
-        dataIndex: 'type',
-        key: 'type',
-        render() {
-            return <>
-                <Select defaultValue="png" style={{width: 120}}>
-                    <Option value="png">png</Option>
-                    <Option value="jpg">jpg</Option>
-                    <Option value="jpeg">jpeg</Option>
-                </Select>
-            </>
-        },
-    },
-    {
-        title: '状态',
-        key: 'status',
-        dataIndex: 'status',
-        render(status: string) {
-            let color = status.length > 5 ? 'geekblue' : 'green';
-            if (status === 'loser') {
-                color = 'volcano';
-            }
-            return (
-                <Tag color={color} key={status}>
-                    {status.toUpperCase()}
-                </Tag>
-            );
-        },
-    }
-];
-
-const data = [
-    {
-        key: '1',
-        name: '1.jpg',
-        status: '未上传',
-    }
-];
+import {ConvertImage} from "./ConvertImage";
 
 const App: FC = () => (
     <Layout>
@@ -62,16 +11,7 @@ const App: FC = () => (
             <h1 className="page-title">在线图像文件转换器</h1>
         </Header>
         <Content className="content">
-            <Button type="primary" danger icon={<UploadOutlined />} size="large" className="choose-button">
-                选择文件
-            </Button>
-            <input type="file" id="upload-add" style={{display: 'none'}}/>
-
-            <Table columns={columns} dataSource={data} className="table" pagination={false}/>
-
-            <Button type="primary" icon={<RightOutlined />} size="large" className="upload-button">
-                开始转换
-            </Button>
+            <ConvertImage/>
         </Content>
     </Layout>
 );
---
+++ view/src/ConvertImage.tsx
@@ -0,0 +1,78 @@
+import React, {RefObject} from "react";
+import {Button, Select, Table, Tag} from "antd";
+import {RightOutlined, UploadOutlined} from "@ant-design/icons";
+import './ConvertImage.less';
+
+export class ConvertImage extends React.Component<any, any> {
+    private readonly uploadAdd: RefObject<HTMLInputElement>
+
+    constructor(props: any) {
+        super(props);
+        this.uploadAdd = React.createRef();
+        this.state = {
+            columns: [
+                {
+                    title: '文件名',
+                    dataIndex: 'name',
+                    key: 'name',
+                    render(text: string) {
+                        return <a href="/#">{text}</a>;
+                    },
+                },
+                {
+                    title: '目标格式',
+                    dataIndex: 'type',
+                    key: 'type',
+                    render() {
+                        return <>
+                            <Select defaultValue="png" style={{width: 120}}>
+                                <Select.Option value="png">png</Select.Option>
+                                <Select.Option value="jpg">jpg</Select.Option>
+                                <Select.Option value="jpeg">jpeg</Select.Option>
+                            </Select>
+                        </>
+                    },
+                },
+                {
+                    title: '状态',
+                    key: 'status',
+                    dataIndex: 'status',
+                    render(status: string) {
+                        let color = status.length > 5 ? 'geekblue' : 'green';
+                        if (status === 'loser') {
+                            color = 'volcano';
+                        }
+                        return (
+                            <Tag color={color} key={status}>
+                                {status.toUpperCase()}
+                            </Tag>
+                        );
+                    },
+                }
+            ],
+            data: [
+                {
+                    key: '1',
+                    name: '1.jpg',
+                    status: '未上传',
+                }
+            ]
+        }
+    }
+
+    render() {
+        return <>
+            <Button type="primary" danger icon={<UploadOutlined/>} size="large" className="choose-button"
+                    onClick={() => this.uploadAdd.current?.click()}>
+                选择文件
+            </Button>
+            <input type="file" ref={this.uploadAdd} style={{display: 'none'}}/>
+
+            <Table columns={this.state.columns} dataSource={this.state.data} className="table" pagination={false}/>
+
+            <Button type="primary" icon={<RightOutlined/>} size="large" className="upload-button">
+                开始转换
+            </Button>
+        </>;
+    }
+}
